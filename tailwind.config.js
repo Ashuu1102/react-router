61e4ff6721b3c59357fcba8011512d38b8408b86
Ashutosh <aa8781@srmist.edu.in>
2024-10-05 18:41:09 +0530

    Half Project Done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Include paths to your source files
+    './public/index.html'               // Include additional paths if necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
